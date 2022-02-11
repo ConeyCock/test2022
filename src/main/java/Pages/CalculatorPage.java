0936e6ce852baa8ba6e82a9b9243ba69f0bd43bf
Evgeniy_Taranov <coneycock@gmail.com>
2022-02-11 13:01:58 +0300

    Создана калькулятор пейдж

---
+++ src/main/java/Pages/CalculatorPage.java
@@ -0,0 +1,67 @@
+package Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import static org.openqa.selenium.support.PageFactory.initElements;
+
+public class CalculatorPage {
+
+// Действия
+    //(
+    @FindBy(css = "div[jsname*='j93WEe']")
+    public WebElement CalculatorInput90;
+
+    //+
+    @FindBy(css = "div[jsname*='XSr6wc']")
+    public WebElement CalculatorInput91;
+
+    //)
+    @FindBy(css = "div[jsname*='qCp9A']")
+    public WebElement CalculatorInput92;
+
+    //*
+    @FindBy(css = "div[jsname*='YovRWb']")
+    public WebElement CalculatorInput93;
+
+    //-
+    @FindBy(css = "div[jsname*='pPHzQc']")
+    public WebElement CalculatorInput94;
+
+    // '/'
+    @FindBy(css = "div[jsname*='WxTTNd']")
+    public WebElement CalculatorInput95;
+
+    //=
+    @FindBy(css = "div[jsname*='Pt8tGc']")
+    public WebElement CalculatorInput96;
+
+
+    // Цифры
+
+    @FindBy(css = "div[jsname*='bkEvMb']")
+    public WebElement CalculatorInput0;
+
+    @FindBy(css = "div[jsname*='N10B9']")
+    public WebElement CalculatorInput1;
+
+    @FindBy(css = "div[jsname*='lVjWed']")
+    public WebElement CalculatorInput2;
+
+    @FindBy(css = "div[jsname*='KN1kY']")
+    public WebElement CalculatorInput3;
+
+    @FindBy(css = "div[jsname*='xAP7E']")
+    public WebElement CalculatorInput4;
+
+    @FindBy(css = "div[jsname*='Ax5wH']")
+    public WebElement CalculatorInput5;
+
+
+
+    public CalculatorPage(WebDriver driver) {
+        initElements(driver, this);
+    }
+
+}
--- src/test/java/tests/main.java
+++ src/test/java/tests/main.java
@@ -1,5 +1,6 @@
 package tests;
 
+import Pages.CalculatorPage;
 import Pages.SearchPage;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -12,6 +13,7 @@ public class main {
 
     private static  WebDriver driver;
     private static SearchPage searchPage;
+    private static CalculatorPage calculatorPage;
 
 
     @BeforeAll
@@ -21,25 +23,27 @@ public class main {
         options.addArguments("start-maximized");
         driver = new ChromeDriver(options);
         searchPage = new SearchPage(driver);
+        calculatorPage = new CalculatorPage(driver);
+
     }
 
     @Test
     public void test1() {
         driver.get("http://google.com");
         searchPage.search("Калькулятор" );
-        driver.findElement(By.cssSelector("div[jsname*='j93WEe']")).click();
-        driver.findElement(By.cssSelector("div[jsname*='N10B9']")).click();
-        driver.findElement(By.cssSelector("div[jsname*='XSr6wc']")).click();
-        driver.findElement(By.cssSelector("div[jsname*='lVjWed']")).click();
-        driver.findElement(By.cssSelector("div[jsname*='qCp9A']")).click();
-        driver.findElement(By.cssSelector("div[jsname*='YovRWb']")).click();
-        driver.findElement(By.cssSelector("div[jsname*='KN1kY']")).click();
-        driver.findElement(By.cssSelector("div[jsname*='pPHzQc']")).click();
-        driver.findElement(By.cssSelector("div[jsname*='xAP7E']")).click();
-        driver.findElement(By.cssSelector("div[jsname*='bkEvMb']")).click();
-        driver.findElement(By.cssSelector("div[jsname*='WxTTNd']")).click();
-        driver.findElement(By.cssSelector("div[jsname*='Ax5wH']")).click();
-        driver.findElement(By.cssSelector("div[jsname*='Pt8tGc']")).click();
+        calculatorPage.CalculatorInput90.click();
+        calculatorPage.CalculatorInput1.click();
+        calculatorPage.CalculatorInput91.click();
+        calculatorPage.CalculatorInput2.click();
+        calculatorPage.CalculatorInput92.click();
+        calculatorPage.CalculatorInput93.click();
+        calculatorPage.CalculatorInput3.click();
+        calculatorPage.CalculatorInput94.click();
+        calculatorPage.CalculatorInput4.click();
+        calculatorPage.CalculatorInput0.click();
+        calculatorPage.CalculatorInput95.click();
+        calculatorPage.CalculatorInput5.click();
+        calculatorPage.CalculatorInput96.click();
 
 
     }
